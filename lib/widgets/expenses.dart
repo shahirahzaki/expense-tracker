113ef080cc9f5e10e92e82f0b39de9a7b38e4532
Nurul Shahirah binti Mohd Zaki <shahirah.zaki@tmrnd.com.my>
2025-05-03 23:20:04 +0800

    ✨ feat: add undo functionality for expense removal in Expenses widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -44,9 +44,29 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(
+                  expenseIndex,
+                  expense,
+                );
+              });
+            }),
+        content: Text('Expense deleted'),
+      ),
+    );
   }
 
   @override
