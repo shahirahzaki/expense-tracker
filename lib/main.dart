2d7baae2cd51db0fab8493ead8ba02ad5eb22bde
Nurul Shahirah binti Mohd Zaki <shahirah.zaki@tmrnd.com.my>
2025-05-04 15:19:18 +0800

    ✨ feat: reorganize theme settings in main app

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,34 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          // scaffoldBackgroundColor: const Color.fromARGB(255, 254, 236, 255),
-          //Instead of setting up an entire Theme from scratch, it's often better to copy an existing Theme
-          //and then just style individual aspects of that Theme.
-          colorScheme: kColourScheme,
-          //You can set up an app bar Theme here and create an app bar Theme object.
-          //Again, best done by creating an object and then using copy with so that you get some default app bar Theme settings by Flutter.
-          //And you then just override selected settings.
-          appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kColourScheme.onPrimaryContainer,
-            foregroundColor: kColourScheme.primaryContainer,
-          )),
+        // scaffoldBackgroundColor: const Color.fromARGB(255, 254, 236, 255),
+        //Instead of setting up an entire Theme from scratch, it's often better to copy an existing Theme
+        //and then just style individual aspects of that Theme.
+        colorScheme: kColourScheme,
+        //You can set up an app bar Theme here and create an app bar Theme object.
+        //Again, best done by creating an object and then using copy with so that you get some default app bar Theme settings by Flutter.
+        //And you then just override selected settings.
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColourScheme.onPrimaryContainer,
+          foregroundColor: kColourScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColourScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColourScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              // titleLarge: ThemeData().textTheme.titleLarge!.copyWith(),
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColourScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
+      ),
       home: Expenses(),
     ),
   );
